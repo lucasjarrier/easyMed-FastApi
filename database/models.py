05d89b90b1b410df414bbf0d3e95054044fc2114
lucasjarrier <lucas.cavalcanti@ccc.ufcg.edu.br>
2023-03-09 11:29:29 -0300

    Intial commit

---
+++ database/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    medications = relationship('Medication', backref='user')
+
+
+class Medication(Base):
+    __tablename__ = 'medication'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    user_id = Column(Integer, ForeignKey('user.id'))
